7c7a2676d12be7ded72ae8cc841c954fbca5ffff
Saiguer10 <eljosemsp@gmail.com>
2025-05-08 17:08:48 +0200

    Subida de repositorios para los modelos

---
+++ src/main/java/com/example/demo/repositories/OrganizerRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.repositories;
+
+import com.example.demo.models.Organizer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrganizerRepository extends JpaRepository<Organizer, Long> {
+}
